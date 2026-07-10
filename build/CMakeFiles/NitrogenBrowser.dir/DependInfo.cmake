ee2e2b71412bd2d97bbd64a4ac70d4c947f7e6b7
Maple (gastown) <Maple@gastown.local>
2026-07-10 18:02:14 +0000

    WIP: container eviction save

---
+++ build/CMakeFiles/NitrogenBrowser.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/browser/Bookmarks.cpp" "CMakeFiles/NitrogenBrowser.dir/src/browser/Bookmarks.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/browser/Bookmarks.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/browser/BrowserUI.cpp" "CMakeFiles/NitrogenBrowser.dir/src/browser/BrowserUI.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/browser/BrowserUI.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/browser/SettingsPanel.cpp" "CMakeFiles/NitrogenBrowser.dir/src/browser/SettingsPanel.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/browser/SettingsPanel.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/browser/Tab.cpp" "CMakeFiles/NitrogenBrowser.dir/src/browser/Tab.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/browser/Tab.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/browser/URLBar.cpp" "CMakeFiles/NitrogenBrowser.dir/src/browser/URLBar.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/browser/URLBar.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/css/CSSParser.cpp" "CMakeFiles/NitrogenBrowser.dir/src/css/CSSParser.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/css/CSSParser.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/devtools/DOMInspector.cpp" "CMakeFiles/NitrogenBrowser.dir/src/devtools/DOMInspector.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/devtools/DOMInspector.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/devtools/PaintProfiler.cpp" "CMakeFiles/NitrogenBrowser.dir/src/devtools/PaintProfiler.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/devtools/PaintProfiler.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/html/DOMNode.cpp" "CMakeFiles/NitrogenBrowser.dir/src/html/DOMNode.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/html/DOMNode.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/html/HTMLParser.cpp" "CMakeFiles/NitrogenBrowser.dir/src/html/HTMLParser.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/html/HTMLParser.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/html/Tokenizer.cpp" "CMakeFiles/NitrogenBrowser.dir/src/html/Tokenizer.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/html/Tokenizer.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/html/TreeBuilder.cpp" "CMakeFiles/NitrogenBrowser.dir/src/html/TreeBuilder.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/html/TreeBuilder.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/js/VM.cpp" "CMakeFiles/NitrogenBrowser.dir/src/js/VM.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/js/VM.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/layout/Box.cpp" "CMakeFiles/NitrogenBrowser.dir/src/layout/Box.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/layout/Box.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/main.cpp" "CMakeFiles/NitrogenBrowser.dir/src/main.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/main.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/net/HttpClient.cpp" "CMakeFiles/NitrogenBrowser.dir/src/net/HttpClient.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/net/HttpClient.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/render/Painter.cpp" "CMakeFiles/NitrogenBrowser.dir/src/render/Painter.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/render/Painter.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/util/Logging.cpp" "CMakeFiles/NitrogenBrowser.dir/src/util/Logging.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/util/Logging.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/util/Memory.cpp" "CMakeFiles/NitrogenBrowser.dir/src/util/Memory.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/util/Memory.cpp.o.d"
+  "/workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/util/String.cpp" "CMakeFiles/NitrogenBrowser.dir/src/util/String.cpp.o" "gcc" "CMakeFiles/NitrogenBrowser.dir/src/util/String.cpp.o.d"
+  "" "NitrogenBrowser" "gcc" "CMakeFiles/NitrogenBrowser.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/NitrogenBrowser.dir/src/util/Memory.cpp.o.d
@@ -0,0 +1,18 @@
+CMakeFiles/NitrogenBrowser.dir/src/util/Memory.cpp.o: \
+ /workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/src/util/Memory.cpp \
+ /usr/include/stdc-predef.h \
+ /workspace/rigs/9202891d-89fd-4630-a477-4b5fac2d6e86/worktrees/gt__maple__e50d8f29/include/util/Memory.h \
+ /usr/include/c++/14/cstddef \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/cpu_defines.h \
+ /usr/include/c++/14/pstl/pstl_config.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/stddef.h \
+ /usr/include/c++/14/bits/version.h
